baca205a0bb766c7cd2b9fa5e1730d8d28167810
SeaLeopard35302 <dd6786263@gmail.com>
2024-10-16 15:25:06 +0530

    Merge sort complete

--- merge-sort.c
+++ merge-sort.c
@@ -4,42 +4,58 @@
 int arr[MAX];
 int temp[MAX];
 
-
 void merge(int low, int mid, int high) {
-  int i=low, x=low, y=mid+1;
-  int k;
-  while(x <= mid && y <= high) {
-    if(arr[x] <= arr[y]) temp[i] = arr[x++];
-    else temp[i] = arr[y++];
+  int i = low, x = low, y = mid + 1;
+  while (x <= mid && y <= high) {
+    if (arr[x] <= arr[y]) {
+      temp[i++] = arr[x++];
+    } else {
+      temp[i++] = arr[y++];
+    }
   }
-  if(x <= mid) {
-    for(k=x;k<=mid;k++) temp[i++] = arr[k];
+  while (x <= mid) {
+    temp[i++] = arr[x++];
   }
-  else {
-    for(k=y;k<=high;k++) temp[i++] = arr[k];
-    for(k=low;k<=high;k++) arr[k] = temp[k];
+  while (y <= high) {
+    temp[i++] = arr[y++];
   }
-}
-void mrgSort(int low, int high) {
-  int mid = (low+high)/2;
-  mrgSort(low, mid);
-  mrgSort(mid+1,high);
-  merge(low, mid, high);
+  for (i = low; i <= high; i++) {
+    arr[i] = temp[i];
+  }
 }
 
-void main() {
+void mrgSort(int low, int high) {
+  if (low < high) {
+    int mid = (low + high) / 2;
+    mrgSort(low, mid);
+    mrgSort(mid + 1, high);
+    merge(low, mid, high);
+  }
+}
+
+int main() {
   int n;
   printf("Enter size of array: \n");
-  scanf("%d",&n);
-  printf("Enter values of array: \n");
-  for(int i=0;i<n;i++) scanf("%d",&arr[i]);
-  int low = 0;
-  int high = n-1;
-  mrgSort(low,high);
+  scanf("%d", &n);
   
-  printf("\n Unsorted array: \n");
-  for(int i=0;i<n;i++) printf("%d",arr[i]);
-  printf("\n Sorted Array: \n");
-  for(int i=0;i<n;i++) printf("%d",arr[i]);
+  if (n > MAX) {
+    printf("Size exceeds maximum limit of %d\n", MAX);
+    return 1;
+  }
+
+  printf("Enter values of array: \n");
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
+
+  mrgSort(0, n - 1);
+  
+  printf("\nSorted Array: \n");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
   printf("\n");
+  
+  return 0;
 }
+
